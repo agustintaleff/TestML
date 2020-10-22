0bfee09dd49cc5b96a4a6a6a22985e02322d8359
agustintaleff <agustintaleff@gmail.com>
2020-10-21 21:49:34 -0300

    commit 11

--- fileRead.js
+++ fileRead.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 fs.readFile('./texto.txt', function (err,datos) {
     if (err) {
-        console.log('Error de lectura 2');
+        console.log('Error de lectura 3');
     } else {
         console.log(datos.toString());
     }
