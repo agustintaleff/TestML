c41a3565088558329a166e3b3519c474deca23d5
agustintaleff <agustintaleff@gmail.com>
2020-10-21 21:22:12 -0300

    Commit 6

--- http.js
+++ http.js
@@ -6,6 +6,7 @@ const ManejadorServer = function (req,res) {
     res.write('<H1>Dani balazo</H1>');
     res.write('<H1>Hola puto 2</H1>');
     res.write('<H1>Agus Sobala</H1>');
+    res.write('<H1>Colorado Ladron</H1>');
     res.end;
 }
 
