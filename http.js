692032aea64094c06725909c814f7f266bae88fe
agustintaleff <agustintaleff@gmail.com>
2020-10-21 21:30:21 -0300

    Comete esta

--- http.js
+++ http.js
@@ -7,7 +7,7 @@ const ManejadorServer = function (req,res) {
     res.write('<H1>Hola puto 2</H1>');
     res.write('<H1>Agus Sobala</H1>');
     res.write('<H1>Colorado Ladron</H1>');
-    res.write('<H1>Semilla Ladron</H1>');
+    res.write('<H1>Semilla Ladronazo</H1>');
     res.end;
 }
 
